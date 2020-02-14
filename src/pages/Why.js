060b62766aecd3a67f58d19a15e745aab94ec600
Faris Osman <farismosman@gmail.com>
2020-02-14 22:25:21 +0200

    adds why koncept content

--- src/pages/Why.js
+++ src/pages/Why.js
@@ -7,59 +7,57 @@ const IndexPage = () => (
     <article id="main">
       <header>
         <h2>Why Koncept</h2>
-        <p>sdifjlsjdifh</p>
+        <p>World class products</p>
       </header>
       <section className="wrapper style5">
         <div className="inner">
-          <h3>Lorem ipsum dolor</h3>
+          <h3>Global Experience</h3>
           <p>
-            Morbi mattis mi consectetur tortor elementum, varius pellentesque
-            velit convallis. Aenean tincidunt lectus auctor mauris maximus, ac
-            scelerisque ipsum tempor. Duis vulputate ex et ex tincidunt, quis
-            lacinia velit aliquet. Duis non efficitur nisi, id malesuada justo.
-            Maecenas sagittis felis ac sagittis semper. Curabitur purus leo,
-            tempus sed finibus eget, fringilla quis risus. Maecenas et lorem
-            quis sem varius sagittis et a est. Maecenas iaculis iaculis sem.
-            Donec vel dolor at arcu tincidunt bibendum. Interdum et malesuada
-            fames ac ante ipsum primis in faucibus. Fusce ut aliquet justo.
-            Donec id neque ipsum. Integer eget ultricies odio. Nam vel ex a orci
-            fringilla tincidunt. Aliquam eleifend ligula non velit accumsan
-            cursus. Etiam ut gravida sapien.
+            Koncept teams consist of seasoned individuals with years of experience locally
+            and internationally. We strive to deliver the best quality following industry
+            standards. Our solutions are offered as an end to end package that caters for
+            the wide needs of our clients.
           </p>
 
           <p>
-            Vestibulum ultrices risus velit, sit amet blandit massa auctor sit
-            amet. Sed eu lectus sem. Phasellus in odio at ipsum porttitor mollis
-            id vel diam. Praesent sit amet posuere risus, eu faucibus lectus.
-            Vivamus ex ligula, tempus pulvinar ipsum in, auctor porta quam.
-            Proin nec dui cursus, posuere dui eget interdum. Fusce lectus magna,
-            sagittis at facilisis vitae, pellentesque at etiam. Quisque posuere
-            leo quis sem commodo, vel scelerisque nisi scelerisque. Suspendisse
-            id quam vel tortor tincidunt suscipit. Nullam auctor orci eu dolor
-            consectetur, interdum ullamcorper ante tincidunt. Mauris felis nec
-            felis elementum varius.
+            Unlike traditional teams, our teams usually consist of a broad spectrum of skills
+            built to meet the needs of our clients, and understand their requirements.
+            For each project we seek to hire domain experts to integrate their knowledge in the
+            solution, and ensure the highest qualities. 
           </p>
 
           <hr />
 
-          <h4>Feugiat aliquam</h4>
+          <h4>Unique Management Style</h4>
           <p>
-            Nam sapien ante, varius in pulvinar vitae, rhoncus id massa. Donec
-            varius ex in mauris ornare, eget euismod urna egestas. Etiam lacinia
-            tempor ipsum, sodales porttitor justo. Aliquam dolor quam, semper in
-            tortor eu, volutpat efficitur quam. Fusce nec fermentum nisl. Aenean
-            erat diam, tempus aliquet erat.
+            Koncept teams value communication and consider it as a critical key for the success.
+            We employ the latest technologies to ensure that everyone on
+            the project is informed in an honest and transparent manner.
+          </p>
+
+          <hr />
+
+          <h4>Business Parteners</h4>
+          <p>
+            We seek to engage with businesses and collaborate to identify their problems,
+            apply our minds to offer the best solution possible to the problem. We consider
+            ourselves as partners to your business rather than outsiders.
           </p>
 
           <p>
-            Etiam iaculis nulla ipsum, et pharetra libero rhoncus ut. Phasellus
-            rutrum cursus velit, eget condimentum nunc blandit vel. In at
-            pulvinar lectus. Morbi diam ante, vulputate et imperdiet eget,
-            fermentum non dolor. Ut eleifend sagittis tincidunt. Sed viverra
-            commodo mi, ac rhoncus justo. Duis neque ligula, elementum ut enim
-            vel, posuere finibus justo. Vivamus facilisis maximus nibh quis
-            pulvinar. Quisque hendrerit in ipsum id tellus facilisis fermentum.
-            Proin mauris dui, at vestibulum sit amet, auctor bibendum neque.
+            At Koncept we believe that the best products are built when people collaborate
+            in healthy, transparent and agile environment. We always ensure that our teams
+            are integrating with the different units of your business to achieve a high standard
+            products.
+          </p>
+
+          <hr />
+
+          <h4>After Service Support</h4>
+          <p>
+            Upon delivering the final solution, we do not simply walk away. We continue to monitor
+            and offer support to ensure the solution in hand is the best possible solution. We pride
+            our brand as the brand of success.
           </p>
         </div>
       </section>
